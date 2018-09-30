9714b6661c983d1578be9cc21047d7679111d794
John Jairo Quitora Sandoval <johnjota82@gmail.com>
2018-09-29 20:54:21 -0500

    Inicio de proyecto

---
+++ cicloviapp/src/pages/hello-ionic/hello-ionic.ts
@@ -0,0 +1,56 @@
+import { Component } from '@angular/core';
+import { Geolocation, Geoposition } from '@ionic-native/geolocation';
+
+declare var google;
+
+@Component({
+  selector: 'page-hello-ionic',
+  templateUrl: 'hello-ionic.html'
+})
+export class HelloIonicPage {
+  map: any;
+  
+  constructor(
+    private geolocation: Geolocation
+  ) {}
+
+  ionViewDidLoad(){
+    this.getPosition();
+  }
+
+  getPosition():any{
+    this.geolocation.getCurrentPosition().then(response => {
+      this.loadMap(response);
+    })
+    .catch(error =>{
+      console.log(error);
+    })
+  }
+
+  loadMap(position: Geoposition){
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+    console.log(latitude, longitude);
+    
+    // create a new map by passing HTMLElement
+    let mapEle: HTMLElement = document.getElementById('map');
+  
+    // create LatLng object
+    let myLatLng = {lat: latitude, lng: longitude};
+  
+    // create map
+    this.map = new google.maps.Map(mapEle, {
+      center: myLatLng,
+      zoom: 12
+    });
+  
+    google.maps.event.addListenerOnce(this.map, 'idle', () => {
+      let marker = new google.maps.Marker({
+        position: myLatLng,
+        map: this.map,
+        title: 'Hello World!'
+      });
+      mapEle.classList.add('show-map');
+    });
+  }
+}
